ebd02d1fa86801c84eaf3a01d83e9da73e69d078
github username <github email address>
2019-10-23 08:54:39 -0700

    menu component created with prelim text

---
+++ src/Components/Menu.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <nav>
+        {" "}
+        Menu component
+        <a
+          href="https://www.getlinus.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Accounts{" "}
+        </a>
+        <a
+          href="https://www.getlinus.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Ledger{" "}
+        </a>
+        <a
+          href="https://www.getlinus.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Finance{" "}
+        </a>
+        <a
+          href="https://www.getlinus.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Learn{" "}
+        </a>
+        <a
+          href="https://www.getlinus.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Marketplace{" "}
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
