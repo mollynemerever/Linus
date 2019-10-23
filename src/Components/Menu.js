0c2edf6685d3dc30bb15b945a762910e5c884577
github username <github email address>
2019-10-23 09:38:55 -0700

    separated out icons from link elements in menu, font and color

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,55 +2,69 @@ import React from "react";
 
 const Menu = () => {
   return (
-    <div>
+    <div className="menu-component">
       <nav>
         {" "}
-        Menu component
-        <a
-          href="https://www.getlinus.io"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="fas fa-user-circle"
-        >
-          {" "}
-          Accounts{" "}
-        </a>
-        <a
-          href="https://www.getlinus.io"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="fas fa-file-invoice-dollar"
-        >
-          {" "}
-          Ledger{" "}
-        </a>
-        <a
-          href="https://www.getlinus.io"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="fas fa-piggy-bank"
-        >
-          {" "}
-          Finance{" "}
-        </a>
-        <a
-          href="https://www.getlinus.io"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="fas fa-book-open"
-        >
-          {" "}
-          Learn{" "}
-        </a>
-        <a
-          href="https://www.getlinus.io"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="fas fa-store"
-        >
-          {" "}
-          Marketplace{" "}
-        </a>
+        Linus
+        <div>
+          <ul>
+            <li>
+              <i class="fas fa-user-circle" />
+              <a
+                href="https://www.getlinus.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Accounts{" "}
+              </a>
+            </li>
+            <li>
+              <i class="fas fa-file-invoice-dollar" />
+              <a
+                href="https://www.getlinus.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Ledger{" "}
+              </a>
+            </li>
+            <li>
+              <i class="fas fa-piggy-bank" />
+              <a
+                href="https://www.getlinus.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Finance{" "}
+              </a>
+            </li>
+            <li>
+              <i class="fas fa-book-open" />
+              <a
+                href="https://www.getlinus.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Learn{" "}
+              </a>
+            </li>
+            <li>
+              <i class="fas fa-store" />
+              <a
+                href="https://www.getlinus.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Marketplace{" "}
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
