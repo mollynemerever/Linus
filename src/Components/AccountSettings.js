e2a8fc51f3e9834e6315cd10baf7dbb61bf5d027
github username <github email address>
2019-10-23 17:35:49 -0700

    prelim_account_settings_component

---
+++ src/Components/AccountSettings.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class AccountSettings extends Component {
+  render() {
+    return (
+      <div className="account-settings">
+        {" "}
+        <header>Account Settings </header>
+        <p>Default Currency </p>
+        <small>
+          {" "}
+          This is the default currency that all of your valuations will be
+          traslated to.{" "}
+        </small>
+        <select>
+          <option value="USD"> USD </option>
+          <option value="CAD"> CAD </option>
+        </select>
+        <p> Accounting Methodology </p>
+        <small>
+          {" "}
+          This is the methodology in which your tax and accoutning documentation
+          will be prepared.{" "}
+        </small>
+        <select>
+          <option value="FIFO"> FIFO </option>
+          <option value="LIFO"> LIFO </option>
+        </select>
+      </div>
+    );
+  }
+}
