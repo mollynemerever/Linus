cc5cff4beef525209793b31ab3044f268a3129d2
github username <github email address>
2019-10-24 15:06:28 -0700

    state tied to toggle button complete

--- src/Components/PrivacySettings.js
+++ src/Components/PrivacySettings.js
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 import "../App.css";
 
 export default class PrivacySettings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      profilePrivacy: true,
+      searchPrivacy: false
+    };
+  }
+
+  //initialize state 1 true & 1 false for sake of demonstrating that check
+  //depends upon state
+
+  updateProfilePrivacy = () => {
+    console.log("inside update profile privacy");
+    this.setState({ profilePrivacy: !this.state.profilePrivacy });
+  };
+
+  updateSearchPrivacy = () => {
+    console.log("inside update search privacy");
+    this.setState({ searchPrivacy: !this.state.searchPrivacy });
+  };
   render() {
     return (
       <div className="privacy-settings">
@@ -12,10 +32,14 @@ export default class PrivacySettings extends Component {
           {" "}
           Your profile with your wallet info will not be public or searchable.{" "}
         </small>
-        <select>
-          <option value="USD"> USD </option>
-          <option value="CAD"> CAD </option>
-        </select>
+        <label class="switch">
+          <input
+            type="checkbox"
+            onClick={this.updateProfilePrivacy}
+            checked={this.state.profilePrivacy}
+          />
+          <span class="slider round" />
+        </label>
         <p> Search Privacy </p>
         <small>
           {" "}
@@ -23,7 +47,12 @@ export default class PrivacySettings extends Component {
           will be prepared.{" "}
         </small>
         <label class="switch">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            class="checkbox"
+            onClick={this.updateSearchPrivacy}
+            checked={this.state.searchPrivacy}
+          />
           <span class="slider round" />
         </label>
       </div>
